a74e6222e1dd60d87a9abfd8d1c2fc1540c641bd
David W Wright <dave.william.wright@gmail.com>
2025-08-01 13:55:55 +0100

    First functional test (FT) specced in comments + update to use unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,44 @@
+import unittest
 from selenium import webdriver
 
-# Next 2 lines are needed to specify the path to your geckodriver
-geckodriver_path = "/snap/bin/geckodriver"
-driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox(service=driver_service)
-# browser = webdriver.Firefox() # Originial Example
-browser.get("http://localhost:8000")
+    def setUp(self):
 
-assert "Congratulations!" in browser.title
-print("OK")
+        # Next 2 lines are needed to specify the path to your geckodriver
+        geckodriver_path = "/snap/bin/geckodriver"
+        driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
+
+        self.browser = webdriver.Firefox(service=driver_service)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+
+        # Edith has heard about a cool new online to-do app. 
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+
+        # When she hits enter, the page updates,and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
+
