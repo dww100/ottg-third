a6d0f23469b1807310d2955cd71317154b407afc
David W Wright <dave.william.wright@gmail.com>
2025-08-01 12:13:37 +0100

    First test, with edits for Ubuntu compatibility from: https://stackoverflow.com/questions/76846675/selenium-ubuntu22-bug-message-binary-is-not-a-firefox-executable

---
+++ functional_tests.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+# Next 2 lines are needed to specify the path to your geckodriver
+geckodriver_path = "/snap/bin/geckodriver"
+driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
+
+browser = webdriver.Firefox(service=driver_service)
+# browser = webdriver.Firefox() # Originial Example
+browser.get("http://localhost:8000")
+
+Assert "Congratulations!" in browser.title
+print("OK")
