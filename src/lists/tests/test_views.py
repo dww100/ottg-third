332fbbb552bcdda66fe134abd0d1f0ff003d3ccf
David W Wright <dave.william.wright@gmail.com>
2025-09-30 17:15:31 +0100

    Split out unit tests into two files

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -69,34 +69,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, "other list item")
 
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-
-        mylist = List()
-        mylist.save()
-
-        item1 = Item()
-        item1.text = "The first (ever) list item"
-        item1.list = mylist
-        item1.save()
-
-        item2 = Item()
-        item2.text = "Item the second"
-        item2.list = mylist
-        item2.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        self.assertEqual(saved_items[0].text, "The first (ever) list item")
-        self.assertEqual(saved_items[0].list, mylist)
-        self.assertEqual(saved_items[1].text, "Item the second")
-        self.assertEqual(saved_items[1].list, mylist)
-
 class NewITemTest(TestCase):
     """
     
