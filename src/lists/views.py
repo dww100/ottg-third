6bb5b95b1b3155c608fafa362d7394a376e1ce6d
David W Wright <dave.william.wright@gmail.com>
2025-10-06 11:30:24 +0100

    Placeholders for using fomr in view + template (not in use yet)

--- src/lists/views.py
+++ src/lists/views.py
@@ -1,10 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
 
+    # return render(request, "home.html", {"form": ItemForm()})
     return render(request, "home.html")
 
 def view_list(request, list_id):
